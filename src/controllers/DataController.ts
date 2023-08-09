8aa347028c52ef4022ea2c4a3db81dc70a9053d2
ijefiN <jeffersonmecmiler@hotmail.com>
2023-08-09 17:37:57 -0300

    Salvando alterações

--- src/controllers/DataController.ts
+++ src/controllers/DataController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { ReceiveNewZeev } from "../services/SendZeevService";
-import Sender from "../Sender";
 
 export default class DataController {
   sendZeev = async (req: Request, res: Response) => {
@@ -8,7 +7,6 @@ export default class DataController {
       title: string;
       userId: number;
     }
-    const sender = new Sender();
 
     const service = new ReceiveNewZeev();
 

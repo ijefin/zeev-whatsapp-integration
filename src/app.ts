8449f9f5b63e56e80b27c291a59c3e4338020aa5
ijefiN <jeffersonmecmiler@hotmail.com>
2023-08-27 12:17:28 -0300

    puppeeter

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import routes from "../routes";
+import { join } from "path";
 
 const app = express();
 const port = 3000;
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

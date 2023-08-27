2f798f443cbd362f44966ae5b825ada9d863bc2e
ijefiN <jeffersonmecmiler@hotmail.com>
2023-08-27 12:07:41 -0300

    configuração extra do puppeteer

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,8 @@
 import express from "express";
 import routes from "../routes";
-import puppeteer from "puppeteer";
 
 const app = express();
 const port = 3000;
-
-const browser = puppeteer.launch({
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

20c0ed8ae6449804818c1e96cf94d1d3157b54a8
ijefiN <jeffersonmecmiler@hotmail.com>
2023-08-29 15:56:14 -0300

    lets :s

--- src/services/WhatsappService.ts
+++ src/services/WhatsappService.ts
@@ -1,35 +1,46 @@
-import { create, Whatsapp } from "venom-bot";
+import puppeteer, { Browser } from "puppeteer";
+import { Whatsapp, create } from "venom-bot"
 
 export default class WhatsappService {
   private client!: Whatsapp;
+  private browser!: Browser;
 
   constructor() {
     this.initialize();
   }
 
   async sendText(to: string, body: string) {
-    //553192310461@c.us
     await this.client.sendText(to, body);
   }
 
-  private initialize() {
-    const qr = (base64QrImg: string) => {
-      base64QrImg;
-    };
+  private async initialize() {
+    try {
+      this.browser = await puppeteer.launch({
+        // executablePath: "/usr/local/bin/chromium-browser", // Atualize com o caminho correto
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
 
-    const status = (statusSession: string) => {
-      return statusSession;
-    };
+      const qr = (base64QrImg: string) => {
+      };
 
-    const start = (client: Whatsapp) => {
-      this.client = client;
+      const status = (statusSession: string) => {
+        return statusSession;
+      };
 
-      // this.sendText( "553192310461@c.us",
-      // "Olá Roberto! O código de confirmação é: *512455*")
-    };
+      const start = (client: Whatsapp) => {
+        this.client = client;
 
-    create("Zeev Notificator", qr, status)
-      .then((client: any) => start(client))
-      .catch((err: any) => console.log(err));
+        // this.sendText( "553192310461@c.us",
+        // "Olá Roberto! O código de confirmação é: *512455*")
+      };
+
+      create("Zeev Notificator", qr, status, {
+        browser: this.browser, // Passa o navegador Puppeteer
+      })
+        .then((client: Whatsapp) => start(client))
+        .catch((err: any) => console.log(err));
+    } catch (error) {
+      console.error("Ocorreu um erro ao iniciar o bot:", error);
+    }
   }
 }
